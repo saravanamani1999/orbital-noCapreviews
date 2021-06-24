f66f286f59ebb68499c3409dcc49d20eeebbadad
wuchuank <wuchuank@gmail.com>
2021-06-24 17:19:14 +0800

    Updated models to include forum and comment schema

--- models/forum.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const ForumSchema = new Schema({
-    module: String,
-    comment: String
-});
-
-module.exports = mongoose.model('Forum', ForumSchema);
