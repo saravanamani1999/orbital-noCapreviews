f97e0e71451d495fbcbdab318c78ceaf9fdc61f2
wuchuank <wuchuank@gmail.comgit config --global user.email wuchuank@gmail.com>
2021-07-26 11:12:29 +0800

    Deployment code

--- static/scripts/autocomplete.js
+++ static/scripts/autocomplete.js
@@ -1,59 +1,53 @@
-
 var options = {
-	url: "https://saravanamani1999.github.io/api/moduleInfo.json",
+  url: "https://saravanamani1999.github.io/api/moduleInfo.json",
 
-	getValue: function(element) {
-		return element.moduleCode; 
-		// + '|' + element.title;
-	},
+  getValue: function (element) {
+    return element.moduleCode;
+    // + '|' + element.title;
+  },
 
-	adjustWidth: false,
+  adjustWidth: false,
 
-	template: {
-		type: "description",
-		fields: {
-			description: "title"
-		}
+  template: {
+    type: "description",
+    fields: {
+      description: "title",
+    },
 
-		// type: "custom",
-		// method: function (value, item) {
-		// 	return value.substr(0, value.indexOf('|'));
-		// }
+    // type: "custom",
+    // method: function (value, item) {
+    // 	return value.substr(0, value.indexOf('|'));
+    // }
+  },
 
-	},
+  list: {
+    maxNumberOfElements: 10,
+    match: {
+      enabled: true,
+    },
+    showAnimation: {
+      type: "slide", //normal|slide|fade
+      time: 300,
+      callback: function () {},
+    },
 
-	list: {
-		maxNumberOfElements: 10,
-		match: {
-			enabled: true
-		},
-		showAnimation: {
-			type: "slide", //normal|slide|fade
-			time: 300,
-			callback: function() {}
-		},
-
-		hideAnimation: {
-			type: "slide", //normal|slide|fade
-			time: 300,
-			callback: function() {}
-		},
-		onClickEvent: function() {
-			
-			var module = $("#search-bar").getSelectedItemData().moduleCode;
-			location.href = "http://localhost:3000/modules/" + module;
-			// location.href = "https://nocap-reviews.herokuapp.com/modules/" + module;	 
-		},
-		onKeyEnterEvent: function() {
-			
-			var selected= $("#search-bar").getSelectedItemData().moduleCode;
-			location.href = "http://localhost:3000/modules/" + selected;
-			// location.href = "https://nocap-reviews.herokuapp.com/modules/" + selected;	 
-		}
-
-	},
-	theme: "round"
+    hideAnimation: {
+      type: "slide", //normal|slide|fade
+      time: 300,
+      callback: function () {},
+    },
+    onClickEvent: function () {
+      var module = $("#search-bar").getSelectedItemData().moduleCode;
+      // location.href = "http://localhost:3000/modules/" + module;
+      location.href = "https://nocap-reviews.herokuapp.com/modules/" + module;
+    },
+    onKeyEnterEvent: function () {
+      var selected = $("#search-bar").getSelectedItemData().moduleCode;
+      // location.href = "http://localhost:3000/modules/" + selected;
+      location.href = "https://nocap-reviews.herokuapp.com/modules/" + selected;
+    },
+  },
+  theme: "round",
 };
 
 $("#search-bar").easyAutocomplete(options);
-  
